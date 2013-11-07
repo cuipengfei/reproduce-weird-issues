6187d259501a87bd4c89d61be611efcff57ab6a3
gituser <twer@tw.com>
2013-11-07 15:28:46 +0800

    init

---
+++ ReproduceMocoIssue/src/main/java/localhost/_12306/hello_world_soap_http/types/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost:12306/hello_world_soap_http/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package localhost._12306.hello_world_soap_http.types;
