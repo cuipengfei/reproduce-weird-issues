c9a4b78b0eb5767bce45a2fc70d163f5e60a8e95
cuipengfei <cuipengfei2008@gmail.com>
2014-05-31 22:06:02 +0800

    coal pile knows how to split itself

--- scala-jacoco-branch-coverage/src/main/scala/cpf/coalboss/CoalPile.scala
+++ scala-jacoco-branch-coverage/src/main/scala/cpf/coalboss/CoalPile.scala
@@ -1,5 +1,7 @@
 package cpf.coalboss
 
 case class CoalPile(weight: Double) {
-
+  def split(splitWeight: Double): (CoalPile, CoalPile) = {
+    (CoalPile(splitWeight), CoalPile(weight - splitWeight))
+  }
 }
