18b73928e71b6d98a17352a11a0e0584ba2d7c85
cuipengfei <cuipengfei2008@gmail.com>
2014-05-31 23:20:35 +0800

    make load one by one method shorter

--- scala-jacoco-branch-coverage/src/main/scala/cpf/coalboss/CoalBoss.scala
+++ scala-jacoco-branch-coverage/src/main/scala/cpf/coalboss/CoalBoss.scala
@@ -12,24 +12,39 @@ object CoalBoss {
     else {
       val lastTruck = trucks.last
       val firstPile = piles.head
-      
+
       if (!lastTruck.isFull) {
         if (lastTruck.spaceLeft == firstPile.weight) {
-          val loadedTruck = lastTruck.load(firstPile)
-          loadOneByOne(piles.tail, trucks.dropRight(1) :+ loadedTruck :+ CoalTruck(List()))
+          loadCurrentTruckAndAddNewEmptyTruck(lastTruck, firstPile, piles, trucks)
         }
         else if (lastTruck.spaceLeft > firstPile.weight) {
-          val notFullyLoadedTruck = lastTruck.load(firstPile)
-          loadOneByOne(piles.tail, trucks.dropRight(1) :+ notFullyLoadedTruck)
+          loadCurrentTruck(lastTruck, firstPile, piles, trucks)
         }
         else {
-          val (splitPile, remainingPile) = firstPile.split(lastTruck.spaceLeft)
-          val loadedTruck = lastTruck.load(splitPile)
-          loadOneByOne(piles.tail :+ remainingPile, trucks.dropRight(1) :+ loadedTruck :+ CoalTruck(List()))
+          splitLoadAndAddNewTruck(lastTruck, firstPile, piles, trucks)
         }
       } else {
         loadOneByOne(piles, trucks :+ CoalTruck(List()))
       }
     }
+  }
+
+  private def splitLoadAndAddNewTruck(lastTruck: CoalTruck, firstPile: CoalPile,
+                                      piles: Seq[CoalPile], trucks: Seq[CoalTruck]): Seq[CoalTruck] = {
+    val (splitPile, remainingPile) = firstPile.split(lastTruck.spaceLeft)
+    val loadedTruck = lastTruck.load(splitPile)
+    loadOneByOne(piles.tail :+ remainingPile, trucks.dropRight(1) :+ loadedTruck :+ CoalTruck(List()))
+  }
+
+  private def loadCurrentTruck(lastTruck: CoalTruck, firstPile: CoalPile,
+                               piles: Seq[CoalPile], trucks: Seq[CoalTruck]): Seq[CoalTruck] = {
+    val notFullyLoadedTruck = lastTruck.load(firstPile)
+    loadOneByOne(piles.tail, trucks.dropRight(1) :+ notFullyLoadedTruck)
+  }
+
+  private def loadCurrentTruckAndAddNewEmptyTruck(lastTruck: CoalTruck, firstPile: CoalPile,
+                                                  piles: Seq[CoalPile], trucks: Seq[CoalTruck]): Seq[CoalTruck] = {
+    val loadedTruck = lastTruck.load(firstPile)
+    loadOneByOne(piles.tail, trucks.dropRight(1) :+ loadedTruck :+ CoalTruck(List()))
   }
 }
