78751ca455ffa5872408a9ce9aa19fa57971cdac
cuipengfei <cuipengfei2008@gmail.com>
2014-06-02 21:38:04 +0800

    truck中包含的pile的数量不用一个一个的弄了

--- scala-jacoco-branch-coverage/src/test/scala/cpf/coalboss/CoalBossSpec.scala
+++ scala-jacoco-branch-coverage/src/test/scala/cpf/coalboss/CoalBossSpec.scala
@@ -12,38 +12,27 @@ class CoalBossSpec extends Specification {
 
       trucks must have size 2
 
-      trucks.head.piles must have size 3
       truckMustContain(trucks.head, 5, 4, 0.9)
-
-      trucks.last.piles must have size 1
       truckMustContain(trucks.last, 2.1)
     }
 
     "load coal piles into trucks when there are big piles" in {
       val trucks = CoalBoss.loadTrucks(List(CoalPile(15), CoalPile(14), CoalPile(13)))
-      println(trucks)
+
       trucks must have size 5
 
-      trucks.head.piles must have size 1
       truckMustContain(trucks.head, 9.9)
-
-      trucks(1).piles must have size 2
       truckMustContain(trucks(1), 5.1, 4.8)
-
-      trucks(2).piles must have size 2
       truckMustContain(trucks(2), 9.2, 0.7)
-
-      trucks(3).piles must have size 1
       truckMustContain(trucks(3), 9.9)
-
-      trucks.last.piles must have size 1
       truckMustContain(trucks.last, 2.4)
     }
 
   }
 
-  private def truckMustContain(truck: CoalTruck, expectedWeights: Double*): Seq[MatchResult[Seq[Double]]] = {
+  private def truckMustContain(truck: CoalTruck, expectedWeights: Double*): Seq[MatchResult[Any]] = {
     val weights = truck.piles.map(_.weight)
-    expectedWeights.map(weight => weights must contain(beCloseTo(weight, 0.01)))
+    (weights.size === truck.piles.size) +:
+      expectedWeights.map(weight => weights must contain(beCloseTo(weight, 0.01)))
   }
 }
