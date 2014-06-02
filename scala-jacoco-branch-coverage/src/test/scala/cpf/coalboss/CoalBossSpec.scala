2ac6ed4d249971424d53273000e9fe1b67740d57
cuipengfei <cuipengfei2008@gmail.com>
2014-06-02 21:01:12 +0800

    nice matcher

--- scala-jacoco-branch-coverage/src/test/scala/cpf/coalboss/CoalBossSpec.scala
+++ scala-jacoco-branch-coverage/src/test/scala/cpf/coalboss/CoalBossSpec.scala
@@ -11,7 +11,7 @@ class CoalBossSpec extends Specification {
 
       trucks.size === 2
 
-      trucks.head.piles.size === 3
+      trucks.head.piles must have size 3
       trucks.head.piles.head.weight must beCloseTo(5, 0.01)
       trucks.head.piles(1).weight must beCloseTo(4, 0.01)
       trucks.head.piles.last.weight must beCloseTo(0.9, 0.01)
